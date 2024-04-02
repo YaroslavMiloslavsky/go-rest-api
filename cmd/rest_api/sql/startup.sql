90e2c9301bf78aced918e0b2f22ef9e97eb2c39d
MiloYa01 <yaroslav.miloslavsky@lexisnexisrisk.com>
2024-04-02 10:29:07 +0300

    added flags for embeding

--- cmd/rest_api/sql/startup.sql
+++ cmd/rest_api/sql/startup.sql
@@ -6,37 +6,3 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP,
     updated_at TIMESTAMP
 );
-
--- Insert statements for the users table
--- Will create a flag to load dummy data only when flag activated
-INSERT INTO users (username, user_age, created_at, updated_at)
-VALUES ('alice', 23, '2024-03-11 07:43:21', '2024-03-11 07:43:21');
-
-INSERT INTO users (username, user_age, created_at, updated_at)
-VALUES ('bob', 42, '2024-03-25 07:43:21', '2024-03-25 07:43:21');
-
-INSERT INTO users (username, user_age, created_at, updated_at)
-VALUES ('charlie', 30, '2024-03-20 07:43:21', '2024-03-20 07:43:21');
-
-INSERT INTO users (username, user_age, created_at, updated_at)
-VALUES ('dave', 28, '2024-03-15 07:43:21', '2024-03-15 07:43:21');
-
-INSERT INTO users (username, user_age, created_at, updated_at)
-VALUES ('eve', 37, '2024-03-29 07:43:21', '2024-03-29 07:43:21');
-
-INSERT INTO users (username, user_age, created_at, updated_at)
-VALUES ('frank', 55, '2024-03-18 07:43:21', '2024-03-18 07:43:21');
-
-INSERT INTO users (username, user_age, created_at, updated_at)
-VALUES ('grace', 33, '2024-03-29 07:43:21', '2024-03-29 07:43:21');
-
-INSERT INTO users (username, user_age, created_at, updated_at)
-VALUES ('helen', 47, '2024-03-14 07:43:21', '2024-03-14 07:43:21');
-
-INSERT INTO users (username, user_age, created_at, updated_at)
-VALUES ('ian', 25, '2024-03-22 07:43:21', '2024-03-22 07:43:21');
-
-INSERT INTO users (username, user_age, created_at, updated_at)
-VALUES ('jane', 31, '2024-03-27 07:43:21', '2024-03-27 07:43:21');
-
-
